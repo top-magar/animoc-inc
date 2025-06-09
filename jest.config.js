b3c0f742dd4b91322a48c73c95748d7e68ebc86c
Top Magar <166315726+top-magar@users.noreply.github.com>
2025-06-09 22:41:27 +0545

    Fix Jest setup and add matchMedia polyfill

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
 };
